57b508f3d7729cd3dfbd1b7791700edcd80f02f4
AntonIT99 <elsass.wolf@gmail.com>
2026-05-22 02:52:43 +0200

    Restore missing localized names for enchantments. Remove getMaxLevel() in EnchantmentJuggernaut since it can inherit super implementation.

--- src/main/java/com/flansmodultimate/common/enchantments/EnchantmentJuggernaut.java
+++ src/main/java/com/flansmodultimate/common/enchantments/EnchantmentJuggernaut.java
@@ -1,7 +1,5 @@
 package com.flansmodultimate.common.enchantments;
 
-import org.jetbrains.annotations.NotNull;
-
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
@@ -10,13 +8,12 @@ public class EnchantmentJuggernaut extends Enchantment
 {
     public EnchantmentJuggernaut()
     {
-        super(Rarity.VERY_RARE, EnchantmentCategory.ARMOR, new EquipmentSlot[] { EquipmentSlot.HEAD, EquipmentSlot.CHEST, EquipmentSlot.LEGS, EquipmentSlot.FEET });
-    }
-
-    @Override
-    public int getMaxLevel()
-    {
-        return 1;
+        super(Rarity.VERY_RARE, EnchantmentCategory.ARMOR, new EquipmentSlot[] {
+            EquipmentSlot.HEAD,
+            EquipmentSlot.CHEST,
+            EquipmentSlot.LEGS,
+            EquipmentSlot.FEET
+        });
     }
 
     @Override
